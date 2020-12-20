3d281c42af7ba9046f23ee585c396c8250c79ab8
Lehady <lxs4287@rit.edu>
2020-12-20 20:17:40 +0100

    Created new Theme
    Created unicorn theme folder that includes the follwoing:
    
    - front-page for the homepage display
    - archive page to hold the blogs created
    - 404 page error to display error page
    - assets folder that will hold js files, images, and css files
    - footer that will display throughout the site
    - functions page that will override default wordpress functionalities
    - header page that will be displayed on every page
    -screenshot image file to be displayed as a thumbnail for our theme
    - single page to display content from a single blog
    - search page to display search results

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">	   
+    <nav class="navbar navbar-expand-lg navbar-light ml-2 ml-md-5">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="<?php echo get_template_directory_uri()."/assets/img/brand/logo.svg"?>" width="50" height="50" alt="">
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+            <div class="navbar-nav mx-auto">
+                <a class="nav-link" href="#">
+                    <img src="<?php echo get_template_directory_uri()."/assets/img/icons/ic-facebook.svg"?>" width="20" height="20" alt="">
+                </a>
+                <a class="nav-link" href="#">
+                    <img src="<?php echo get_template_directory_uri()."/assets/img/icons/ic-instagram.svg"?>" width="20" height="20" alt="">
+                </a>
+                <a class="nav-link" href="#">
+                    <img src="<?php echo get_template_directory_uri()."/assets/img/icons/ic-twitter.svg"?>" width="20" height="20" alt="">
+                </a>
+            </div>
+            <span class="navbar-text">
+                2020. All love and happines
+            </span>
+        </div>
+    </nav>	   
+</footer>
+    
+    </div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,37 @@
+<?php
+
+function unicorn_theme_support(){
+    //adds dynamic title tag support
+    add_theme_support('title-tag');
+}
+add_action( 'after_setup_theme', 'unicorn_theme_support' );
+
+function unicorn_menus(){
+    $menu_setup = array(
+        'primary'   => "Desktop primary Top Menu",
+        'footer'    => "Footer Menu Items"
+    );
+
+    register_nav_menus($menu_setup);
+}
+add_action( 'init', 'unicorn_menus' );
+
+function unicorn_register_styles(){
+    $version = wp_get_theme()->get( 'Version' );
+
+    wp_enqueue_style( 'unicorn-style', get_template_directory_uri() . '/style.css', array('unicorn-bootstrap'), $version, 'all');
+    wp_enqueue_style( 'unicorn-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css", array(), '4.5.3', 'all');
+    wp_enqueue_style( 'unicorn-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
+}
+add_action( 'wp_enqueue_scripts', 'unicorn_register_styles' );
+
+function unicorn_register_scripts(){
+    wp_enqueue_script( 'unicorn-jquery', "https://code.jquery.com/jquery-3.5.1.slim.min.js", array(), '3.5.1', true);
+    wp_enqueue_script( 'unicorn-popper', "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js", array(), '1.16.1', true);
+    wp_enqueue_script( 'unicorn-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js", array(), '4.5.3', true);
+    wp_enqueue_script( 'unicorn-js', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0', true);
+
+}
+add_action( 'wp_enqueue_scripts', 'unicorn_register_scripts' );
+
+?>
